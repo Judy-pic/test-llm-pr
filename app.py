0801f16431347ef2720515623cdd62b618f5f45e
Judy-pic <104192364+Judy-pic@users.noreply.github.com>
2024-10-15 14:46:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    test()
     return "Hello, Flask!"
+
+def test():
+    num1 = 10
+    num2 = 20
+    sum = num1 + num2
+    print(f"Sum: {sum}")
+
+    multiplied = num1 * num2
+    print(f"Product: {multiplied}")
 
 if __name__ == '__main__':
     app.run(debug=True)
